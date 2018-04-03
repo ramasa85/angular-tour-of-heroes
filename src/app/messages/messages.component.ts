ad5f3b8f425ed2f101a914f95aeb7feb8acb230f
ramasa85 <ramasa85@gmail.com>
2018-04-03 09:24:05 +0000

    ramasa85

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MessageService } from '../message.service';
 
 @Component({
-  selector: 'app-message',
+  selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.css']
 })
